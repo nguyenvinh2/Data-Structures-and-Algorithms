0447604290b7ec7bf3b5588936e3740e203f8865
nguye <nguyenv2@outlook.com>
2018-10-13 22:26:28 -0700

    challenge completed

---
+++ Challenges/fibonacciSequence/fibonnaciSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace fibonnaciSequence
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      int input = 45;
+      Console.WriteLine(input);
+      Console.WriteLine(Fibonacci(input));
+      Console.ReadKey();
+    }
+
+    public static int Fibonacci(int input)
+    {
+      int fibonnaci = 1;
+      int priorValue = 1;
+      int placeholder;
+      if (input == 0)
+      {
+        return 0;
+      }
+      if (input == 1)
+      {
+        return 1;
+      }
+      for (int i = 2; i < input; i++)
+      {
+        placeholder = fibonnaci;
+        fibonnaci += priorValue;
+        priorValue = placeholder;
+      }
+      return fibonnaci;
+    }
+  }
+}
