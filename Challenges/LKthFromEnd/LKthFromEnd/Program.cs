d528fae18b1ce26084b6c7059d90d992592e368e
nguye <nguyenv2@outlook.com>
2018-10-16 15:44:37 -0700

    function completed

--- Challenges/LKthFromEnd/LKthFromEnd/Program.cs
+++ Challenges/LKthFromEnd/LKthFromEnd/Program.cs
@@ -7,11 +7,14 @@ namespace LKthFromEnd
   {
     static void Main(string[] args)
     {
+      int input = 5;
       Console.WriteLine("Hello World!");
-      KthSearch(5);
+      Console.WriteLine($"Input: {input}");
+      KthSearch(input);
+      Console.ReadKey();
     }
 
-    public static void KthSearch(int input)
+    public static Node KthSearch(int input)
     {
       LinkList InputList = new LinkList();
       InputList.Append(new Node("Something"));
@@ -19,18 +22,47 @@ namespace LKthFromEnd
       InputList.Append(new Node("Gone"));
       InputList.Append(new Node("Wrong"));
       InputList.Append(new Node("Charlie"));
-
+      InputList.Append(new Node("Dan"));
+      InputList.Append(new Node("Stan"));
       InputList.printAllNodes();
-      Console.ReadKey();
-      int counter = 0;
 
+      int length = 0;
       Node current = InputList.Head;
       while (current != null)
       {
-        counter++;
+        length++;
+        current = current.Next;
       }
 
+      try
+      {
+        NumberCheck(input, length);
+        current = InputList.Head;
+        for (int i = 0; i < length - input - 1; i++)
+        {
+          current = current.Next;
+        }
+        Console.WriteLine($"The Node at Position {input} from the end is {current.Data}");
+        return current;
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new Node(null);
+      }
+    
+    }
 
+    public static void NumberCheck(int input, int length)
+    {
+      if (input >= length)
+      {
+        throw new Exception("Your Input must not be greater than the length of the List.");
+      }
+      else if (input < 0)
+      {
+        throw new Exception("Your Input must not be negative.");
+      }
     }
   }
 }
