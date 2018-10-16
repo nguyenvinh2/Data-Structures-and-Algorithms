e264a54d6825a6b68ffc967ded5c0015f7416db1
nguye <nguyenv2@outlook.com>
2018-10-16 14:59:23 -0700

    Next can't be used

--- Challenges/LKthFromEnd/LKthFromEnd/Program.cs
+++ Challenges/LKthFromEnd/LKthFromEnd/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using LinkedList;
 
 namespace LKthFromEnd
 {
-  class Program
+  public class Program
   {
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      KthSearch(5);
+    }
+
+    public static void KthSearch(int input)
+    {
+      LinkList InputList = new LinkList();
+      InputList.Append(new Node("Something"));
+      InputList.Append(new Node("Has"));
+      InputList.Append(new Node("Gone"));
+      InputList.Append(new Node("Wrong"));
+      InputList.Append(new Node("Charlie"));
+
+      InputList.printAllNodes();
+      Console.ReadKey();
+      int counter = 0;
+
+      Node current = InputList.Head;
+      while (current != null)
+      {
+        counter++;
+      }
+
+
     }
   }
 }
