593de48d78530446a219b762779e5935f6dc8376
nguye <nguyenv2@outlook.com>
2018-10-17 19:54:04 -0700

    scaffold

---
+++ Challenges/llMerge/llMerge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LinkedList;
+
+namespace llMerge
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    public static LinkList Merge(LinkList One, LinkList Two)
+    {
+      LinkList Result = new LinkList();
+      return Result;
+    }
+  }
+}
