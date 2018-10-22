849281868dd4a2fe32264e68dd674796b61db8c9
nguye <nguyenv2@outlook.com>
2018-10-22 09:52:24 -0700

    classes done

---
+++ Data Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue.Classes
+{
+  /// <summary>
+  /// same node as from linked list
+  /// </summary>
+  public class Node
+  {
+    /// <summary>
+    /// node data property
+    /// </summary>
+    public object Data { get; set; }
+    /// <summary>
+    /// node next reference property
+    /// </summary>
+    public Node Next { get; set; }
+    /// <summary>
+    /// for instatiating Node
+    /// </summary>
+    /// <param name="data">object data user input</param>
+    public Node(object data)
+    {
+      Data = data;
+    }
+  }
+}
