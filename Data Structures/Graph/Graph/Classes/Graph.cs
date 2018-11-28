e9e3c00c1dc850c77c5ffa7276816b5a63a039b4
nguye <nguyenv2@outlook.com>
2018-11-28 01:03:44 -0800

    methods complete

--- Data Structures/Graph/Graph/Classes/Graph.cs
+++ Data Structures/Graph/Graph/Classes/Graph.cs
@@ -13,34 +13,103 @@ namespace Graph.Classes
             AdjacencyList = new LinkedList<LinkedList<Tuple<Node, int>>>();
         }
         /// <summary>
-        /// 
+        /// assumes graph is not empty and the parent node exists for the child node to be attached to
+        /// finds the parent node and adds to the linked list
+        /// if child node doesn't already exist, adds a new vertices to the graph
         /// </summary>
-        /// <param name="parent"></param>
-        /// <param name="child"></param>
+        /// <param name="parent">target node to attach to</param>
+        /// <param name="child">node to attach</param>
         public void AddEdge(Node parent, Tuple<Node, int> child)
         {
-            bool listExists = false;
+            if (AdjacencyList.Count != 0)
+            {
+                bool parentExists = false;
+                bool childExists = false;
+                foreach (var item in AdjacencyList)
+                {
+                    var node = item.First.Value.Item1;
+                    if (node.Value == parent.Value)
+                    {
+                        item.AddLast(child);
+                        parentExists = true;
+                    }
+                    if (node.Value == child.Item1.Value)
+                    {
+                        item.AddLast(new Tuple<Node, int>(parent, child.Item2));
+                        childExists = true;
+                    }
+                }
+                if (childExists == false && parentExists == true)
+                {
+                    LinkedList<Tuple<Node, int>> originList = new LinkedList<Tuple<Node, int>>();
+                    Tuple<Node, int> origin = new Tuple<Node, int>(child.Item1, 0);
+                    Tuple<Node, int> parentAdd = new Tuple<Node, int>(parent, child.Item2);
+                    originList.AddFirst(origin);
+                    originList.AddLast(parentAdd);
+                    AdjacencyList.AddLast(originList);
+                }
+            }
+        }
+        /// <summary>
+        /// iterates 2 dimensionally through adjacency to collect the nodes
+        /// </summary>
+        /// <returns>list of all nodes in graph</returns>
+        public List<Node> GetNodes()
+        {
+            List<Node> NodeList = new List<Node>();
+
+            foreach (var yList in AdjacencyList)
+            {
+                foreach (var xList in yList)
+                {
+                    NodeList.Add(xList.Item1);
+                }
+            }
+
+            return NodeList;
+        }
+        /// <summary>
+        /// iterates through graph in y direction, finds the target node
+        /// collects all connecting nodes in that linked list
+        /// </summary>
+        /// <param name="target">node to get neighbor nodes</param>
+        /// <returns>list of neighbor nodes</returns>
+        public List<Tuple<Node, int>> GetNeighbors(Node target)
+        {
+            List<Tuple<Node, int>> neighborNodes = new List<Tuple<Node, int>>();
             foreach (var item in AdjacencyList)
             {
                 var node = item.First.Value.Item1;
-                if (node.Value == parent.Value)
+                if (node.Value == target.Value)
                 {
-                    item.AddLast(child);
-                }
-                if (node.Value == child.Item1.Value)
-                { 
-                    item.AddLast(new Tuple<Node, int>(parent, child.Item2));
-                    listExists = true;
+                    int count = 0;
+                    foreach (var tuplesToAdd in item)
+                    {
+                        if (count > 0)
+                        {
+                            neighborNodes.Add(tuplesToAdd);
+                        }
+                        count++;
+                    }
                 }
             }
-            if (listExists == false || AdjacencyList.Count == 0)
+            return neighborNodes;
+        }
+        /// <summary>
+        /// counts number of nodes in the y direction
+        /// </summary>
+        /// <returns>count of nodes</returns>
+        public uint Size()
+        {
+            uint nodeCount = 0;
+
+            foreach (var yList in AdjacencyList)
             {
-                LinkedList<Tuple<Node, int>> originList = new LinkedList<Tuple<Node, int>>();
-                Tuple<Node, int> origin = new Tuple<Node, int>(child.Item1, 0);
-                originList.AddFirst(origin);
-                AdjacencyList.AddFirst(originList);
+                nodeCount++;
+
             }
 
+            return nodeCount;
         }
     }
 }
--- Data Structures/Graph/Graph/Program.cs
+++ Data Structures/Graph/Graph/Program.cs
@@ -13,16 +13,50 @@ namespace Graph
             Node parent = new Node();
             parent.Value = "A";
 
+            LinkedList<Tuple<Node, int>> originList = new LinkedList<Tuple<Node, int>>();
+            Tuple<Node, int> origin = new Tuple<Node, int>(parent, 0);
+            originList.AddFirst(origin);
+
+            graph.AdjacencyList.AddFirst(originList);
+
             Node child = new Node();
             child.Value = "B";
 
             graph.AddEdge(parent, new Tuple<Node, int>(child, 5));
 
+            Node c = new Node();
+            c.Value = "C";
+
+            graph.AddEdge(parent, new Tuple<Node, int>(c, 4));
+
 
             foreach (var item in graph.AdjacencyList)
             {
-                Console.WriteLine(item.First.Value.Item1.Value);
+                foreach (var node in item)
+                {
+                    Console.Write(node.Item2);
+                    Console.WriteLine(node.Item1.Value);
+                }
             }
+
+            var nodeList = graph.GetNodes();
+
+            foreach (var item in nodeList)
+            {
+                Console.Write(item.Value);
+            }
+
+            var neighbor = graph.GetNeighbors(parent);
+
+            foreach (var item in neighbor)
+            {
+                Console.Write(item.Item1.Value);
+                Console.WriteLine(item.Item2);
+            }
+
+            var count = graph.Size();
+
+            Console.WriteLine(count);
             Console.ReadKey();
         }
     }
