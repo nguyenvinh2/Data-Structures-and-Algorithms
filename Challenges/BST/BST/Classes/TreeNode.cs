b29a1fe2e4bc6b10cfde2d5a05b2bc0ee9a8b8f0
nguye <nguyenv2@outlook.com>
2019-01-28 13:53:39 -0800

    check if BST

---
+++ Challenges/BST/BST/Classes/TreeNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST.Classes
+{
+    public class TreeNode
+    {
+        public TreeNode Left { get; set; } 
+        public TreeNode Right { get; set; }
+        public int Value { get; set; }
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+
+
+    }
+
+    public class Tree
+    {
+        public TreeNode Root { get; set; }
+
+        public Tree(int value)
+        {
+            Root = new TreeNode(value);
+            Root.Left = null;
+            Root.Right = null;
+        }
+
+        public bool BST()
+        {
+            return BSTCheck(Root, int.MinValue, int.MaxValue);
+        }
+
+        public bool BSTCheck(TreeNode Node, int minValue, int maxValue)
+        {
+            if (Node == null)
+            {
+                return true;
+            }
+            if (Node.Value < minValue || Node.Value > maxValue)
+            {
+                return false;
+            }
+            return (BSTCheck(Node.Left, minValue, Node.Value - 1) && BSTCheck(Node.Right, Node.Value + 1, maxValue));
+        }
+    }
+
+}
---
+++ Challenges/BST/BST/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BST.Classes;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Tree TestTree = new Tree(10);
+
+            TestTree.Root.Left = new TreeNode(8);
+            TestTree.Root.Right = new TreeNode(11);
+
+            TestTree.Root.Left.Left = new TreeNode(3);
+            TestTree.Root.Left.Right = new TreeNode(15);
+
+            Console.WriteLine(TestTree.BST());
+
+            Console.ReadKey();
+        }
+    }
+}
