ea449e1489c665d42fdcd8522189e7bc0b4ed721
nguye <nguyenv2@outlook.com>
2019-02-21 10:39:39 -0800

    initial setup

---
+++ Challenges/SuperBalanceTree/SuperBalanceTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SuperBalanceTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool SuperBalance(BinaryTreeNode Root)
+        {
+            return false;
+        }
+    }
+
+
+    class BinaryTreeNode
+    {
+        int Value { get; set; }
+        BinaryTreeNode Left { get; set; }
+        BinaryTreeNode Right { get; set; }
+        BinaryTreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
